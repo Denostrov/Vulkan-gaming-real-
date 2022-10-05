9eda2f3f6246fb5d5a370444e8d1a3d887336e82
Dennis <whyarewestillherejust@gmail.com>
2022-10-06 00:00:29 +0300

    insane templaterino

--- Vulkan gaming(real)/VulkanResources.cpp
+++ Vulkan gaming(real)/VulkanResources.cpp
@@ -2,6 +2,72 @@
 
 using namespace std::literals;
 
+template<class Resource>
+auto enumerateFunc() {}
+template<>
+auto enumerateFunc<vk::ExtensionProperties>() { return vk::enumerateInstanceExtensionProperties(); }
+template<>
+auto enumerateFunc<vk::LayerProperties>() { return vk::enumerateInstanceLayerProperties(); }
+
+template<class Resource>
+auto enumerateRequiredFunc() {}
+template<>
+auto enumerateRequiredFunc<vk::ExtensionProperties>()
+{
+	uint32_t glfwExtensionCount = 0;
+	auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	auto glfwExtensionsVector = std::vector<char const*>(glfwExtensions, glfwExtensions + glfwExtensionCount);
+	if (ENABLE_VALIDATION_LAYERS)
+	{
+		glfwExtensionsVector.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	}
+	return glfwExtensionsVector;
+}
+template<>
+auto enumerateRequiredFunc<vk::LayerProperties>()
+{
+	return std::vector<char const*>(VALIDATION_LAYERS.begin(), VALIDATION_LAYERS.end());
+}
+
+template<class Resource>
+auto getResourceNameFunc(Resource const& val) {}
+template<>
+auto getResourceNameFunc(vk::ExtensionProperties const& val)
+{
+	return val.extensionName;
+}
+template<>
+auto getResourceNameFunc(vk::LayerProperties const& val)
+{
+	return val.layerName;
+}
+
+template<class Resource>
+auto checkAndPrintRequired(std::vector<Resource> const& resources, std::vector<char const*> const& requiredResourceNames)
+{
+	std::cout << getTotalString(requiredResourceNames, "required"s, getFormatString(resources));
+	for (auto name : requiredResourceNames)
+	{
+		bool isSupported = checkVectorContainsString(resources, getResourceNameFunc<Resource>, name);
+		formatPrint(std::cout, "\t{}\t"s + (isSupported ? "(supported)"s : "(not supported)"s) + "\n"s, name);
+		assert(isSupported);
+	}
+}
+
+template<class Resource>
+auto getRequiredResources()
+{
+	auto resources = enumerateFunc<Resource>();
+
+	std::cout << getTotalString(resources, "available"s) << resources;
+
+	auto requiredResources = enumerateRequiredFunc<Resource>();
+	checkAndPrintRequired<Resource>(resources, requiredResources);
+
+	return requiredResources;
+}
+
+
 VulkanResources::VulkanResources()
 	:windowContext(),
 	renderWindow(800, 600, windowContext)
@@ -10,69 +76,18 @@ VulkanResources::VulkanResources()
 	auto vkGetInstanceProcAddr = dynamicLoader.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
 	VULKAN_HPP_DEFAULT_DISPATCHER.init(vkGetInstanceProcAddr);
 
-	auto getRequiredLayers = []()
-	{
-		auto layerProperties = vk::enumerateInstanceLayerProperties();
-		printConsole("{} available layers:\n"sv, layerProperties.size());
-		for (auto const& property : layerProperties)
-		{
-			printLabelValuePairs(std::cout,
-				LabelValuePair{"Layer name"s, property.layerName},
-				LabelValuePair{"Version"s, property.implementationVersion},
-				LabelValuePair{"Description"s, property.description});
-		}
-
-		printConsole("{} required layers:\n"sv, VALIDATION_LAYERS.size());
-		for (auto const& layer : VALIDATION_LAYERS)
-		{
-			bool isSupported = checkVectorContainsString(layerProperties, [](vk::LayerProperties const& p) { return p.layerName; }, layer);
-			printConsole("\t{}\t"s + (isSupported ? "(supported)"s : "(not supported)"s) + "\n"s, layer);
-			assert(isSupported);
-		}
-		return std::vector<char const*>(VALIDATION_LAYERS.begin(), VALIDATION_LAYERS.end());
-	};
-
-	auto getRequiredInstanceExtensions = []()
-	{
-		auto extensionProperties = vk::enumerateInstanceExtensionProperties();
-		printConsole("{} available instance extensions:\n"sv, extensionProperties.size());
-		for (auto const& property : extensionProperties)
-		{
-			printLabelValuePairs(std::cout,
-				LabelValuePair{"Extension name"s, property.extensionName},
-				LabelValuePair{"Version"s, property.specVersion});
-		}
-
-		uint32_t glfwExtensionCount = 0;
-		auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-		auto glfwExtensionsVector = std::vector<char const*>(glfwExtensions, glfwExtensions + glfwExtensionCount);
-		if (ENABLE_VALIDATION_LAYERS)
-		{
-			glfwExtensionsVector.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-		}
-
-		printConsole("{} required instance extensions:\n"sv, glfwExtensionsVector.size());
-		for (auto const& extension : glfwExtensionsVector)
-		{
-			bool isSupported = checkVectorContainsString(extensionProperties, [](vk::ExtensionProperties const& p) { return p.extensionName; }, extension);
-			printConsole("\t{}\t"s + (isSupported ? "(supported)"s : "(not supported)"s) + "\n"s, extension);
-			assert(isSupported);
-		}
-		return glfwExtensionsVector;
-	};
-
 	auto getDebugUtilsMessengerCreateInfo = []()
 	{
 		using enum vk::DebugUtilsMessageTypeFlagBitsEXT;
 		using enum vk::DebugUtilsMessageSeverityFlagBitsEXT;
 		vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfo({}, eError | eWarning | eInfo | eVerbose,
-			eGeneral | eValidation | ePerformance, debugCallback, nullptr);
+																		   eGeneral | eValidation | ePerformance, debugCallback, nullptr);
 		return debugUtilsMessengerCreateInfo;
 	};
 
 	vk::ApplicationInfo applicationInfo{"Copesweeper", VK_MAKE_API_VERSION(0, 1, 0, 0), "Vulkan engine", VK_MAKE_API_VERSION(0, 1, 0, 0), VK_API_VERSION_1_3};
-	auto validationLayers = ENABLE_VALIDATION_LAYERS ? getRequiredLayers() : std::vector<char const*>();
-	auto glfwExtensions = getRequiredInstanceExtensions();
+	auto validationLayers = ENABLE_VALIDATION_LAYERS ? getRequiredResources<vk::LayerProperties>() : std::vector<char const*>();
+	auto glfwExtensions = getRequiredResources<vk::ExtensionProperties>();
 	auto debugUtilsMessengerCreateInfo = getDebugUtilsMessengerCreateInfo();
 	vk::InstanceCreateInfo instanceCreateInfo{{}, &applicationInfo, validationLayers, glfwExtensions, ENABLE_VALIDATION_LAYERS ? &debugUtilsMessengerCreateInfo : nullptr};
 	instance = vk::createInstanceUnique(instanceCreateInfo);
@@ -85,7 +100,7 @@ VulkanResources::VulkanResources()
 		assert(debugMessenger);
 	}
 
-
+	auto physicalDevices = instance->enumeratePhysicalDevices();
 }
 
 VulkanResources::~VulkanResources()
--- Vulkan gaming(real)/print.h
+++ Vulkan gaming(real)/print.h
@@ -5,10 +5,14 @@
 #include <string>
 #include <string_view>
 
+#include "constants.h"
+
+using namespace std::literals;
+
 template<class... Args>
-inline void printConsole(std::string_view format, Args&&... args)
+inline void formatPrint(std::ostream& ostr, std::string_view format, Args&&... args)
 {
-	std::cout << std::vformat(format, std::make_format_args(std::forward<Args>(args)...));
+	ostr << std::vformat(format, std::make_format_args(std::forward<Args>(args)...));
 }
 
 template<class T>
@@ -22,13 +26,50 @@ struct LabelValuePair
 	T value;
 };
 
-inline void printLabelValuePairs(std::ostream& ostr)
-{}
+inline std::string getLabelValuePairsString() { return ""s; }
 template<class T, class... Pairs>
-inline void printLabelValuePairs(std::ostream& ostr, LabelValuePair<T> const& pair, Pairs&&... pairs)
+inline std::string getLabelValuePairsString(LabelValuePair<T> const& pair, Pairs&&... pairs)
 {
-	using namespace std::literals;
+	return std::vformat("\t"s + pair.label + ": {}"s + (sizeof...(pairs) != 0 ? ","s : "\n"), std::make_format_args(pair.value)) +
+		getLabelValuePairsString(std::forward<Pairs>(pairs)...);
+}
 
-	ostr << std::vformat("\t"s + pair.label + ": {}"s + (sizeof...(pairs) != 0 ? ","s : "\n"), std::make_format_args(pair.value));
-	printLabelValuePairs(ostr, std::forward<Pairs>(pairs)...);
+inline auto getFormatString(vk::ExtensionProperties const& val)
+{
+	return getLabelValuePairsString(LabelValuePair{"Extension name"s, val.extensionName},
+									LabelValuePair{"Version"s, val.specVersion});
+}
+inline auto getFormatString(vk::LayerProperties const& val)
+{
+	return getLabelValuePairsString(LabelValuePair{"Extension name"s, val.layerName},
+									LabelValuePair{"Version"s, val.implementationVersion},
+									LabelValuePair{"Description"s, val.description});
+}
+inline auto getFormatString(std::vector<vk::ExtensionProperties> const& vals)
+{
+	return "Instance extension"s + (vals.size() != 1 ? "s"s : ""s);
+}
+inline auto getFormatString(std::vector<vk::LayerProperties> const& vals)
+{
+	return "Layer"s + (vals.size() != 1 ? "s"s : ""s);
+}
+
+template<class T>
+inline auto getTotalString(std::vector<T> const& vals, std::string const& title = {})
+{
+	return std::vformat("{} "s + title + " "s + getFormatString(vals) + ":\n"s, std::make_format_args(vals.size()));
+}
+inline auto getTotalString(std::vector<char const*> const& vals, std::string const& title = {}, std::string const& resourceName = {})
+{
+	return std::vformat("{} "s + title + " "s + resourceName + ":\n"s, std::make_format_args(vals.size()));
+}
+
+template<class T>
+inline std::ostream& operator<<(std::ostream& ostr, std::vector<T> const& values)
+{
+	for (auto const& val : values)
+	{
+		ostr << getFormatString(val);
+	}
+	return ostr;
 }
